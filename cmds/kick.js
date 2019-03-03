a110405b4093418280fe82730346461e5a15ca87
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-02 22:26:16 -0600

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 const config = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
-  throw new Error("piss");
+  client.emit("error", "Unhandled niggatry");
 }
 
 module.exports.help = {
