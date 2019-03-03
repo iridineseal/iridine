d104ae4dcc0d8d3644c9056ff8eb86d27de74bdb
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-02 22:09:32 -0600

    Delete guildMemberRemove.js

--- events/guildMemberRemove.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client, guild, member) => {
-if(guild.id == "486275841148583946") {
-	client.channels.get("538219921088184330").send(member.username + " couldnt handle the pings!!!");
-}
-}
