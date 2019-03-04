d2bac8b7154b154ef7b3c96b171f58fa3dcc3ce7
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-03 19:08:00 -0600

    Update uptime.js

--- cmds/uptime.js
+++ cmds/uptime.js
@@ -10,6 +10,13 @@ function msTime(milliseconds){
         var s = Math.floor(seconds);
         return `${d > 0 ? `${d} Day${d > 1 ? 's, ' : ''}` : ''}${h > 0 ? `${h} Hour${h > 1 ? 's' : ''}` : ''}${m > 0 ? `${h > 0 ? ', ' : ''}${m} Minute${m > 1 ? 's' : ''}` : ''}${s > 0 ? `${m > 0 ? ', ' : ''}${s} Second${s > 1 ? 's' : ''}` : ''}`;
     }
+        const embed = {
+    "description": `${msTime(client.uptime)}`,
+    "color": client.resolver.resolveColor('RANDOM'),
+    "footer": {},
+     fields: []
+  };
+        message.channel.send({ embed });
 }
 
 module.exports.help = {
