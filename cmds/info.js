d557e2527a936d0fca44aa9ee6abb864f4191565
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 16:39:42 -0600

    Update info.js

--- cmds/info.js
+++ cmds/info.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
-message.channel.send(new Discord.RichEmbed().setDescription("Iridine").setColor(client.resolver.resolveColor('RANDOM')).addField('Created by:', 'seal#0222 and Servers#3450'));
+message.channel.send(new Discord.RichEmbed().setTitle("Iridine").setDescription('').setColor(client.resolver.resolveColor('RANDOM')).addField('Created by:', 'seal#0222 and Servers#3450'));
 }
 
 module.exports.help = {
