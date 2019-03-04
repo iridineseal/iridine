74ab4b4b00fb45294df57a658b846755b741ffb4
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 11:50:28 -0600

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,5 +1,11 @@
 module.exports.run = async (client, message, args) => {
-
+const embed = {
+    "description": `last <user> - lastfm status of specified user\nkick <user> <reason> - kicks mentioned user from the guild\n`,
+    "color": client.resolver.resolveColor('RANDOM'),
+    "footer": {},
+     fields: []
+  };
+        message.channel.send({ embed });
 }
 
 module.exports.help = {
