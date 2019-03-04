258b558f3e6bf8c9084ea3d0b87b09799234ad83
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 09:23:24 -0600

    Update message.js

--- events/message.js
+++ events/message.js
@@ -2,7 +2,6 @@ module.exports = async (client, message) => {
 let messageArray = message.content.split(" ");
 let cmd = messageArray[0];
 if(message.author.bot) return;
-if (message.channel.type === "dm") return;
 if(message.content.indexOf(client.config.prefix) !== 0) return;
 const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
