18280739c5c05b1f51f48292bf744989c62ebb7e
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 16:12:27 -0600

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -1,8 +1,25 @@
 const discord = require('discord.js');
-const config = require("../config.json");
 const chalk = require("chalk");
 module.exports.run = async (client, message, args) => {
-message.author.send(message.guild.iconURL);
+const nouser = await new Discord.RichEmbed().setDescription("You need to mention a user to ban.").setColor(client.resolver.resolveColor('RANDOM'));
+const noperms = await new Discord.RichEmbed().setDescription("I don’t have permission to ban this user.").setColor(client.resolver.resolveColor('RANDOM'));
+const noperms2 = await new Discord.RichEmbed().setDescription("You are missing the permission `BAN_MEMBERS`.").setColor(client.resolver.resolveColor('RANDOM'));
+const staff = await new Discord.RichEmbed().setDescription("The mentioned user is a staff member.").setColor(client.resolver.resolveColor('RANDOM'));
+let mentioned = message.mentions.members.first() || message.guild.members.get(args[0]);
+let reason = args.slice(1).join(' ') || 'No reason provided';
+const ban = await new Discord.RichEmbed().setDescription(`${mentioned.user.tag} was banned by ${message.author.toString()} for ${reason}`).setColor(client.resolver.resolveColor('RANDOM'));
+
+if(message.member.hasPermission('BAN_MEMBERS') != true) return message.channel.send(noperms2);
+if(!mentioned) return message.channel.send(nouser);
+if(mentioned.bannable != true) return message.channel.send(noperms);
+if(mentioned.hasPermission('BAN_MEMBERS')) return message.channel.send(staff);
+try {
+mentioned.ban(reason);
+message.channel.send(ban);
+}
+catch(e) {
+return message.channel.send(noperms.setDescription(e));
+}
 }
 
 module.exports.help = {
