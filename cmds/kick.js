85222ff7716dc37f2e5afc5f0272210c53fa6220
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 15:24:38 -0600

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -5,7 +5,7 @@ const noperms = await new Discord.RichEmbed().setDescription("I don’t have per
 const noperms2 = await new Discord.RichEmbed().setDescription("You are missing the permission `KICK_MEMBERS`.").setColor(client.resolver.resolveColor('RANDOM'));
 const staff = await new Discord.RichEmbed().setDescription("The mentioned user is a staff member.").setColor(client.resolver.resolveColor('RANDOM'));
 let mentioned = message.mentions.members.first();
-let reason = args.slice(1).join(' ');
+let reason = args.slice(1).join(' ') || 'No reason provided';
 const kick = await new Discord.RichEmbed().setDescription(`${mentioned.user.tag} was kicked by ${message.author.toString()} for ${reason}`).setColor(client.resolver.resolveColor('RANDOM'));
 
 if(message.member.hasPermission('KICK_MEMBERS') != true) return message.channel.send(noperms2);
