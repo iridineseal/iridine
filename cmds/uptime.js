cd722fe81228558330f8864ceb1b8376115772ba
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-03 19:05:40 -0600

    Update uptime.js

--- cmds/uptime.js
+++ cmds/uptime.js
@@ -1,5 +1,15 @@
 module.exports.run = async (client, message, args) => {
-  
+function msTime(milliseconds){
+        var days = (milliseconds / (1000*60*60)) / 24;
+        var d = Math.floor(days);
+        var hours = (days - d) * 24;
+        var h = Math.floor(hours);
+        var minutes = (hours - h) * 60;
+        var m = Math.floor(minutes);
+        var seconds = (minutes - m) * 60;
+        var s = Math.floor(seconds);
+        return `${d > 0 ? `${d} Day${d > 1 ? 's, ' : ''}` : ''}${h > 0 ? `${h} Hour${h > 1 ? 's' : ''}` : ''}${m > 0 ? `${h > 0 ? ', ' : ''}${m} Minute${m > 1 ? 's' : ''}` : ''}${s > 0 ? `${m > 0 ? ', ' : ''}${s} Second${s > 1 ? 's' : ''}` : ''}`;
+    }
 }
 
 module.exports.help = {
