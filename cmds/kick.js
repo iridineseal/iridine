74e260240d70178edd02be2c4cb70aabd9e0b9b0
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 15:03:27 -0600

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args) => {
 const nouser = await new Discord.RichEmbed().setDescription("You need to mention a user to kick.").setColor(client.resolver.resolveColor('RANDOM'));
 const noperms = await new Discord.RichEmbed().setDescription("I don’t have permission to kick this user.").setColor(client.resolver.resolveColor('RANDOM'));
 const staff = await new Discord.RichEmbed().setDescription("The mentioned user is a staff member.").setColor(client.resolver.resolveColor('RANDOM'));
-let mentioned = message.mentions.users.first();
+let mentioned = message.mentions.members.first();
 
 if(!mentioned) return message.channel.send(nouser);
 if(mentioned.kickable != true) return message.channel.send(noperms);
