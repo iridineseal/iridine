de489e62d75eeffbd0819a1bce9515b258d4d011
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 16:16:22 -0600

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args) => {
 const embed = {
-    "description": `last <user> - lastfm status of specified user\nkick <user> <reason> - kicks mentioned user from the guild\nuserinfo <user> - info about the mentioned user account\nuptime - current bot uptime\nping - current bot latency`,
+    "description": `last <user> - lastfm status of specified user\nkick <user> <reason> - kicks mentioned user from the guild\nban <user> <reason> - bans mentioned user from the guild\nuserinfo <user> - info about the mentioned user account\nuptime - current bot uptime\nping - current bot latency`,
     "color": client.resolver.resolveColor('RANDOM'),
     "footer": {},
      fields: []
