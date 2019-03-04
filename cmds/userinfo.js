574372baddf1852e7eea6006e032aa4f6a69320f
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-04 16:20:42 -0600

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,17 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  let eeee = message.mentions.members.first();
+  let eeee = message.mentions.members.first() || message.member;
   
   const otherembed = new Discord.RichEmbed().setDescription("").setColor("FF4887").setThumbnail(eeee.user.avatarURL).addField("Username", eeee.user.username).addField("Registered at", eeee.user.createdAt).addField("Joined", eeee.joinedAt);
-  const serverembed = new Discord.RichEmbed().setDescription("").setColor("FF4887").setThumbnail(message.author.avatarURL).addField("Username", message.author.username).addField("Registered at", message.author.createdAt).addField("Joined", message.member.joinedAt);
-  
-  if(message.mentions.members.first() !== undefined) {
-  return await message.channel.send(otherembed);
-  }
-  else {
-  return message.channel.send(serverembed);
-  }
+ 
+return message.channel.send(otherembed);
 }
 
 module.exports.help = {
