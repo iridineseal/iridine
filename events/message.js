7ad3aedf41503d2a22a49e7df96e86586d45a225
iridineseal <48167572+iridineseal@users.noreply.github.com>
2019-03-05 18:35:57 -0600

    Update message.js

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@ module.exports = async (client, message) => {
 let messageArray = message.content.split(" ");
 let cmd = messageArray[0];
 if(message.author.bot) return;
-if(message.content.indexOf(client.config.prefix) !== 0) return;
+if(message.content.toLowerCase().indexOf(client.config.prefix) !== 0) return;
 const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
 let commandfile = client.commands.get(cmd.slice(client.config.prefix.length));
